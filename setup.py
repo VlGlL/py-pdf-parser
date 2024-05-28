c8e5105bb18b61568a464e64d156754d61044e34
SomaHelik <129663811+VlGlL@users.noreply.github.com>
2024-05-28 15:47:37 +0200

    chore: updated pyvoronoi version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "dev": [
             "matplotlib==3.5.1",
             "pillow==9.2.0",
-            "pyvoronoi==1.0.7",
+            "pyvoronoi==1.1.0",
             "shapely==1.8.2",
         ],
         "test": [
